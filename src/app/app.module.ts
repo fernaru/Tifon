dfaf1fc7c0a7cb2e2fb4ebe23f04dfe1d72ef88e
Fernando Prado <edisson.prado@vasslatam.com>
2019-03-21 17:43:07 -0500

    menu-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuAdminComponent
   ],
   imports: [
     BrowserModule,
